c05327868dfe726e42379f1dd1ba0f20a1f8ebfc
unknown <johncarlolipa@gmail.com>
2023-08-02 23:38:59 +0800

    added card listing

--- index.js
+++ index.js
@@ -1,43 +1,41 @@
-const express = require('express')
-const expressLayouts = require('express-ejs-layouts')
-const path = require('path')
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const path = require("path");
 
-const app = express()
+const app = express();
 
-app.use(express.static('public'))
-app.use(expressLayouts)
-app.set('layout', './layouts/master')
-app.set('view engine', 'ejs')
+app.use(express.static("public"));
+app.use(expressLayouts);
+app.set("layout", "./layouts/master");
+app.set("view engine", "ejs");
 
-app.get('/', function (req, res) {
-  res.render('pages/index')
-})
+app.get("/", function (req, res) {
+  res.render("pages/index");
+});
 
-app.get('/ben', function (req, res) {
-  res.send('ben')
-})
+app.get("/home", function (req, res) {
+  res.render("pages/home");
+});
 
-app.get('/about', function (req, res) {
-  res.render('pages/about', {
+app.get("/services", function (req, res) {
+  res.render("pages/services");
+});
+
+app.get("/about", function (req, res) {
+  res.render("pages/about", {
     people: [
       {
-        name: 'Lee',
+        name: "Lee",
       },
       {
-        name: 'John',
+        name: "John",
       },
     ],
-  })
-})
+  });
+});
 
-app.get('/login', function (req, res) {
-  res.render('pages/login', {
-    layout: './layouts/authentication',
-  })
-})
-
-app.get("/dashboard", function (req, res) {
-  res.render("pages/dashboard", {
+app.get("/login", function (req, res) {
+  res.render("pages/login", {
     layout: "./layouts/authentication",
   });
 });
