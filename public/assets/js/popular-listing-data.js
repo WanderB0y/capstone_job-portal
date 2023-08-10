f9a4dfe771226daeb29e9bb689970f08bc428c5a
Rovic Villaralvo <102729133+kazewanderboy@users.noreply.github.com>
2023-08-10 15:07:27 +0800

    hdf

---
+++ public/assets/js/popular-listing-data.js
@@ -0,0 +1,12 @@
+let popularInformation = [
+    {
+        img: "Tae",
+
+        position: "Tubero",
+        recruiter: "Lalimbahay",
+        location: "Quezon City",
+
+        salary: "PHP 250 per day",
+        perks: "Free Food",
+    }
+] 
